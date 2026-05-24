179bf706537781726a638e1397740420d43f2f5c
Haadi <lonehaadi81@gmail.com>
2026-05-24 17:00:43 +0530

    done

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,12 @@
-User-Agent: *
-Allow: /
-Disallow: /admin/
-
-Sitemap: https://www.thecareeradvisors.in/sitemap.xml
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/admin/', '/api/'],
+    },
+    sitemap: 'https://www.thecareeradvisors.in/sitemap.xml',
+  }
+}

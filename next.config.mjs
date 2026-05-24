418991cb40cfea3123d23783df71d71f7d7250db
Haadi <lonehaadi81@gmail.com>
2026-05-24 20:41:02 +0530

    donea

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'i.pravatar.cc' }, 
       { protocol: 'https', hostname: 'cdn.vectorstock.com' }, 
       { protocol: 'https', hostname: 'www.transparenttextures.com' },
-      { protocol: 'https', hostname: 'upload.wikimedia.org' }, // Allowed Wikimedia for flags
+      { protocol: 'https', hostname: 'upload.wikimedia.org' }, 
     ],
   },
   
@@ -24,9 +24,9 @@ const nextConfig = {
           { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
           { 
             // UPDATED CONTENT SECURITY POLICY
-            // Added https://upload.wikimedia.org to the img-src list
+            // Added https://content-firebaseappcheck.googleapis.com and https://www.google.com to connect-src
             key: 'Content-Security-Policy', 
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://res.cloudinary.com https://firebasestorage.googleapis.com https://lh3.googleusercontent.com https://i.pravatar.cc https://cdn.vectorstock.com https://www.transparenttextures.com https://upload.wikimedia.org; media-src 'self' https://res.cloudinary.com https://videos.pexels.com; connect-src 'self' https://firestore.googleapis.com https://identitytoolkit.googleapis.com; frame-src 'self' https://www.google.com/recaptcha/ https://recaptcha.google.com/recaptcha/ https://maps.google.com https://www.google.com/maps/ http://googleusercontent.com;" 
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://res.cloudinary.com https://firebasestorage.googleapis.com https://lh3.googleusercontent.com https://i.pravatar.cc https://cdn.vectorstock.com https://www.transparenttextures.com https://upload.wikimedia.org; media-src 'self' https://res.cloudinary.com https://videos.pexels.com; connect-src 'self' https://firestore.googleapis.com https://identitytoolkit.googleapis.com https://content-firebaseappcheck.googleapis.com https://www.google.com; frame-src 'self' https://www.google.com/recaptcha/ https://recaptcha.google.com/recaptcha/ https://maps.google.com https://www.google.com/maps/ http://googleusercontent.com;" 
           }
         ],
       },
